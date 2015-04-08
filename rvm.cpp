334aee94dd678df79042c847d679153212cb7765
Xiong Ding <xiong@hard.physics.gatech.edu>
2015-04-08 16:57:27 -0400

    major progress

--- rvm.cpp
+++ rvm.cpp
@@ -1,37 +1,112 @@
 #include "rvm.h"
 
 rvm_t rvm_init(const char *directory){
+    // need to consider the case when directory already exists.
     rvm_t rvm = new Rvmt(directory);  
     
     return rvm;              //return the rvm
 }
 
+// procedure:
+// 1 check for redo log, if it exists, then apply it to both the segment
+//   on disk and in memory
+// 2 load the content of the disk segment to memory.
+// 
 void *rvm_map(rvm_t rvm, const char *segname, int size_to_create){
+    if(size_to_create <= 0) return (void*) -1;
 
-    bool doesFind = rvm->find(segname);
-    if(doesFind){
-	rvm->createSeg(size_to_create);
+    Segment* seg = rvm->find_by_name(segname);
+    if(seg == NULL){
+	Segment* newSeg = rvm->create_seg(segname, size_to_create);
+	write_seg(newSeg, size_to_create);
+	// return new allocated memory
+	return &(newSeg->content[0]);
     }
     else{
-	rvm->enlarge(size_to_create);
+	fprintf(stderr, "cannot map the same segment twice ! \n");
+	exit(-1);
     }
-    
-    return newMem; // return new allocated memory
 }
 
 void rvm_unmap(rvm_t rvm, void *segbase) {
-    
-    bool doesFind = rvm->find(segbase);
-    if(doesFind){
-	
+    Segment* seg = rvm->find_by_ptr(segbase);
+    if(seg != NULL){
+	rvm->delete_seg(seg);
     }
     else{
-	
+	fprintf(sterr, "unmap non-existing segment !\n");
+	exit(-1);
     }
+}
+
+// two cases :
+// 1. find the segname, then remove the segments in memory and disk
+// 2. not find the segname, rm the segment on disk.
+// 3. not find the segname, and also not on disk, return.
+void rvm_destroy(rvm_t rvm, const char *segname) {
+
     
 }
 
-void rvm_destroy(rvm_t rvm, const char *segname) {
+trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void **segbases){
+    Transaction *trans = new Transaction;
+    for (int i = 0; i < numsegs; i++) {
+	Segment* seg = rvm->find_by_ptr(segbases[i]);
+	if(seg == NULL){
+	    fprintf(sterr, "segment not exist !\n");
+	    exit(-1);
+	}
+	else{
+	    if(seg->beingModified){
+		fprintf(sterr, "segment is under transaction !\n");
+		return -1;
+	    }
+	    else{
+		seg->beingModified = true;
+		Log log;
+		trans.emplace(seg->ptr, log);
+	    }
+	}
+    }
     
-    
+    return trans;
 }
+
+/**
+ * Three cases:
+ * 1. transaction not exist
+ * 2. segment does not belong to this transaction
+ * 3. It is there => create the undo log
+ */
+void rvm_about_to_modify(trans_t tid, void *segbase, int offset, int size){
+    if(tid == -1 || tid == NULL){
+	fprintf(sterr, "transaction does not exist !\n");
+	exit(-1);
+    }
+    else {
+	Transaction::iterator =tid->find(segbase);
+	if ( iterator == tid->end()){
+	    fprintf(sterr, "segment does not belong to this transaction !\n");
+	    exit(-1);
+	}	
+	else {
+	    Log undoLog(offset, std::string(segbase+offset, size));
+	    iterator->second.push_back(undoLog);
+	}
+    }
+}
+
+void rvm_commit_trans(trans_t tid){
+    if(tid == -1 || tid == NULL){
+	fprintf(sterr, "transaction does not exist !\n");
+	exit(-1);
+    }
+    else {
+	
+	
+    }
+}
+void rvm_abort_trans(trans_t tid);
+void rvm_truncate_log(rvm_t rvm);
+
+
--- rvm_internal.h
+++ rvm_internal.h
@@ -1,27 +1,104 @@
 #include <vector>
 #include <string>
 
+typedef struct {
+    std::string seg;		/* segment in memory */
+    std::string log;		/* log */
+    std::string copy;
+} Content;
+
+
+typedef struct {
+    std::string name;
+    std::string content;
+    void *ptr;
+    bool beingModified;
+} Segment;
+
+typedef std::unordered_map<std::string, Segment*> SegNameMap;
+typedef std::unordered_map<void*, Segment*> SegPtrMap;
+
+typedef std::pair<int, std::string> Log;
+typedef std::vector<std::pair<int, std::string> > Log;
+typedef std::unordered_map<void*, std::vector<Log> > Transaction;
+
 
 class Rvmt { 
  public:    
     std::string directory;	/* the directory name */
-    std::vector<std::string> segs; /* array of existing segments */
-    
- Rvmt(const char *dir) : directory(dir) {
+    SegNameMap seg_name; /* array of existing segments */
+    SegPtrMap seg_ptr;
+        
+    Rvmt(const char *dir) : directory(dir) {
 	// if the directory already exists, then mkdir return -1
 	mkdir(dir, 0755); //create directory for log files
     }
     
     ~Rvmt(){}   
     
-    bool find(std::string segname){
-	std::vector<std::string>::iterator = 
-	    std::find(segs.begin(), segs.end(), segname);
-	if(iterator == segs.end()) return false;
-	else return true;
+    Segment* find_by_name(std::string segname){
+	SegNameMap::iterator = seg_name.find(segname);
+	if(iterator == segs.end()) return NULL;
+	else return iterator->second;
+    }
+
+    Segment* find_by_ptr(void *p){
+	SegPtrMap::iterator = seg_ptr.find(p);
+	if(iterator == segs.end()) return NULL;
+	else return iterator->second;
+    }
+    
+    /* load content in sgement on disk to memeory, and insert the entry in
+     * to hash table
+     */
+    Segment* create_seg(std::string segname, int size){
+	Segment* seg = new Segment; 
+	(seg->content).resize(size);
+	seg_name.emplace (segname, seg); /* insert new entry to hash table */
+	seg_ptr.emplace (&(seg->content[0]), seg);
+	
+	return seg;
+    }
+    
+    void delete_seg(Segment *seg){
+	std::string segname = seg->name;
+	void *p = seg->ptr;
+	seg_name.erase(segname);
+	seg_ptr.erase(p);
+	delete seg;
+    }
+
+    void write_seg(Segment *seg, int size_to_create){
+	std::fstream file;
+	file.open(directory + seg->name, std::ifstream::in | std::ifstream::out);
+
+	if (file) { // successfully open the file
+	    /* get length of file: */
+	    file.seekg (0, file.end); 
+	    int length = file.tellg();
+	    file.seekg (0, file.beg);
+	    
+	    if (length < size_to_create){
+		/* set pos to size-1 => after writing one char, we get size */
+		file.seekp(size_to_create-1);
+		char c = '\0';
+		file.write(&c, 1);
+	    }
+	    
+	    assert ( seg->content.size() == size_to_create);
+	    file.read(&(seg->content[0]), size_to_create);
+	    file.close();
+	    
+	    //std::cout.write (&(seg->content[0]), size_to_create);
+	}
+	else{
+	    fprintf(stderr, "cannot open the file %s\n", file);
+	    exit(-1);
+	}
     }
     
     
 };
 
 typedef *Rvmt rvm_t;		/* rvmt points to _rvm_t  */
+typedef Transaction* trans_t;
